1e8bf3018c26e6a5c5df221cbe6914e2eef3ee6e
Tim <tim.tannert@tu-dortmund.de>
2019-08-28 16:11:30 +0200

    Next test

--- dbot/src/main.rs
+++ dbot/src/main.rs
@@ -1,58 +1,45 @@
-use std::env;
-
-use serenity::{
-    model::{channel::Message, gateway::Ready},
-    prelude::*,
+use serenity::client::Client;
+use serenity::model::channel::Message;
+use serenity::prelude::{EventHandler, Context};
+use serenity::framework::standard::{
+    StandardFramework,
+    CommandResult,
+    macros::{
+        command,
+        group
+    }
 };
+
+group!({
+    name: "general",
+    options: {},
+    commands: [ping],
+});
+
+use std::env;
 
 struct Handler;
 
-impl EventHandler for Handler {
-    // Set a handler for the `message` event - so that whenever a new message
-    // is received - the closure (or function) passed will be called.
-    //
-    // Event handlers are dispatched through a threadpool, and so multiple
-    // events can be dispatched simultaneously.
-    fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            // Sending a message can fail, due to a network error, an
-            // authentication error, or lack of permissions to post in the
-            // channel, so log to stdout when some error happens, with a
-            // description of it.
-            println!("Hello World");
-            
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!") {
-                println!("Error sending message: {:?}", why);
-            }
-        }
-    }
+impl EventHandler for Handler {}
 
-    // Set a handler to be called on the `ready` event. This is called when a
-    // shard is booted, and a READY payload is sent by Discord. This payload
-    // contains data like the current user's guild Ids, current user data,
-    // private channels, and more.
-    //
-    // In this case, just print what the current user's username is.
-    fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
+fn main() {
+    // Login with a bot token from the environment
+    let mut client = Client::new(&env::var("TOKEN").expect("token"), Handler)
+        .expect("Error creating client");
+        
+    client.with_framework(StandardFramework::new()
+        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
+        .group(&GENERAL_GROUP));
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start() {
+        println!("An error occurred while running the client: {:?}", why);
     }
 }
 
-fn main() {
-    // Configure the client with your Discord bot token in the environment.
-    let token = env::var("TOKEN")
-        .expect("Expected a token in the environment");
+#[command]
+fn ping(ctx: &mut Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!")?;
 
-    // Create a new instance of the Client, logging in as a bot. This will
-    // automatically prepend your bot token with "Bot ", which is a requirement
-    // by Discord for bot users.
-    let mut client = Client::new(&token, Handler).expect("Err creating client");
-
-    // Finally, start a single shard, and start listening to events.
-    //
-    // Shards will automatically attempt to reconnect, and will perform
-    // exponential backoff until it reconnects.
-    if let Err(why) = client.start() {
-        println!("Client error: {:?}", why);
-    }
+    Ok(())
 }
